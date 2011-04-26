ae14ed76516ef1b8ceb2630b7568da4393824ddb
Kyle Florence <kyle.florence@gmail.com>
2011-04-26 12:23:54 -0700

    Meet jQuery Core Style Guidelines

--- src/jquery.deserialize.js
+++ src/jquery.deserialize.js
@@ -1,104 +1,103 @@
 /**
- *  jQuery.deserialize
+ *	jQuery.deserialize
  *
- *  @author Kyle Florence <kyle[dot]florence[at]gmail[dot]com>
- *  @website https://github.com/kflorence/jquery-deserialize/
- *  @version 1.0.0
+ *	@author Kyle Florence <kyle[dot]florence[at]gmail[dot]com>
+ *	@website https://github.com/kflorence/jquery-deserialize/
+ *	@version 1.0.0
  *
- *  Dual licensed under the MIT and BSD licenses.
+ *	Dual licensed under the MIT and GPLv2 licenses.
  */
-;(function($) {
-  var rcheck = /^(radio|checkbox)$/i,
-      rselect = /^(option|select-one|select-multiple)$/i,
-      rvalue = /^(hidden|text|search|tel|url|email|password|datetime|date|month|week|time|datetime-local|number|range|color|submit|image|reset|button|textarea)$/i;
+(function( jQuery ) {
 
-  // Adds name/value pairs to an array
-  function addItem(name, value) {
-    var i = 0, length = this.length;
+var rcheck = /^(radio|checkbox)$/i,
+		rselect = /^(option|select-one|select-multiple)$/i,
+		rvalue = /^(hidden|text|search|tel|url|email|password|datetime|date|month|week|time|datetime-local|number|range|color|submit|image|reset|button|textarea)$/i;
 
-    if ($.isArray(value)) {
-      for (; i < length; i++) {
-        this.push({ name: name, value: value[i] });
-      }
-    } else {
-      this.push({ name: name, value: value });
-    }
-  }
+// Adds name/value pairs to an array
+function addItem( name, value ) {
+	var i = 0,
+			length = this.length;
 
-  $.fn.deserialize = function(data, callback) {
-    if (!this.length || !data) {
-      return this;
-    }
+	if ( $.isArray( value ) ) {
+		for ( ; i < length; i++ ) {
+			this.push({ name: name, value: value[i] });
+		}
+	} else {
+		this.push({ name: name, value: value });
+	}
+}
 
-    var i,
-        length,
-        dataType = $.type(data),
-        elements = this[0].elements || this.find(":input").get(),
-        normalized = [];
+jQuery.fn.extend({
+	deserialize: function( data, callback ) {
+		if ( !this.length || !data ) {
+			return this;
+		}
 
-    if (dataType === "string") {
-      data = decodeURIComponent(data).split("&");
+		var i, length,
+				elements = this[ 0 ].elements || this.find( ":input" ).get(),
+				normalized = [];
 
-      for (i = 0, length = data.length; i < length; i++) {
-        addItem.apply(normalized, data[i].split("="));
-      }
-    } else if (dataType === "array") {
-      normalized = data;
-    } else if (dataType === "object") {
-      var key;
+		if ( jQuery.isArray( data ) ) {
+			normalized = data;
+		} else if ( jQuery.isPlainObject( data ) ) {
+			var key;
 
-      for (key in data) {
-        if (data.hasOwnProperty(key)) {
-          addItem.call(normalized, key, data[key]);
-        }
-      }
-    }
+			for ( key in data ) {
+				addItem.call( normalized, key, data[ key ] );
+			}
+		} else if ( typeof data === "string" ) {
+			data = decodeURIComponent( data ).split ( "&" );
 
-    if (!elements || !(length = normalized.length)) {
-      return this;
-    }
+			for ( i = 0, length = data.length; i < length; i++ ) {
+				addItem.apply( normalized, data[ i ].split( "=" ) );
+			}
+		}
 
-    var current,
-        element,
-        item,
-        j,
-        len,
-        property,
-        type;
+		if ( !elements || !( length = normalized.length ) ) {
+			return this;
+		}
 
-    for (i = 0; i < length; i++) {
-      current = normalized[i];
+		var current, element, item, j, len, property, type;
 
-      if (!(element = elements[current.name])) {
-        continue;
-      }
+		for ( i = 0; i < length; i++ ) {
+			current = normalized[ i ];
 
-      type = (len = element.length) ? element[0] : element;
-      type = type.type || type.tagName;
+			if ( !( element = elements[ current.name ] ) ) {
+				continue;
+			}
 
-      property = null;
-      rvalue.test(type) && (property = "value");
-      rcheck.test(type) && (property = "checked");
-      rselect.test(type) && (property = "selected");
+			type = ( len = element.length ) ? element[ 0 ] : element;
+			type = type.type || type.nodeName;
+			property = null;
 
-      // Handle element group
-      if (len) {
-        for (j = 0; j < len; j++) {
-          item = element[j];
+			if ( rvalue.test( type ) ) {
+				property = "value";
+			} else if ( rcheck.test( type ) ) {
+				property = "checked";
+			} else if ( rselect.test( type ) ) {
+				property = "selected";
+			}
 
-          if (item.value == current.value) {
-            item[property] = true;
-          }
-        }
-      } else {
-        element[property] = current.value;
-      }
-    }
+			// Handle element group
+			if ( len ) {
+				for ( j = 0; j < len; j++ ) {
+					item = element [j ];
 
-    if ($.isFunction(callback)) {
-      callback.call(this);
-    }
+					if ( item.value == current.value ) {
+						item[ property ] = true;
+					}
+				}
+			} else {
+				element[ property ] = current.value;
+			}
+		}
 
-    return this;
-  };
-})(jQuery);
+		if ( jQuery.isFunction( callback ) ) {
+			callback.call( this );
+		}
+
+		return this;
+	}
+});
+
+})( jQuery );
