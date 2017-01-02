8770e9c6a9a75484acb5cfe591883d6206ff560e
Kyle Florence <kyle.florence@gmail.com>
2017-01-02 10:46:12 -0800

    #35 Update package to include appropriate files.

--- dist/jquery.deserialize.min.js
+++ dist/jquery.deserialize.min.js
@@ -1,7 +1,7 @@
 /**
  * @author Kyle Florence <kyle[dot]florence[at]gmail[dot]com>
  * @website https://github.com/kflorence/jquery-deserialize/
- * @version 1.3.3
+ * @version 1.3.4
  *
  * Dual licensed under the MIT and GPLv2 licenses.
  */
