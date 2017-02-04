d5eab235f58c98d85f795390a222fdd337b60123
Kyle Florence <kyle.florence@gmail.com>
2017-02-04 09:59:53 -0800

    1.3.6

--- src/jquery.deserialize.js
+++ src/jquery.deserialize.js
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * @website https://github.com/kflorence/jquery-deserialize/
- * @version 1.3.5
+ * @version 1.3.6
  *
  * Dual licensed under the MIT and GPLv2 licenses.
  */
